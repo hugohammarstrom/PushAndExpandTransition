741e95a0233578f74e44ae57bdc924105ce273ed
Hugo Hammarström <hugo.hammarstrom@icloud.com>
2016-08-27 23:56:07 +0200

    Initial Commit

---
+++ PushAndExpandTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushAndExpandTransition
+//
+//  Created by Hugo Hammarström on 2016-08-27.
+//  Copyright © 2016 Hugo Hammarström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
